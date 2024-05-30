b3da9ed6652b56a1a37f442aff43091003ce6281
CrazyTea <lipatow.nikita@yandex.ru>
2024-05-30 23:23:24 +0300

    клининг

--- resources/js/components/pages/MonkeyDisplay2.vue
+++ resources/js/components/pages/MonkeyDisplay2.vue
@@ -1,6 +1,10 @@
 <script setup>
 
 import MonkeyMonitor2 from "@/components/organisms/MonkeyMonitor2.vue";
+import {useExperiment2Store} from "@/store/experiment2Store.js";
+import {computed} from "vue";
+const experimentStore = useExperiment2Store()
+const isActive = computed(() => experimentStore.getActive)
 
 
 let w = window.innerWidth;
@@ -11,7 +15,7 @@ let h = window.innerHeight
 <template>
 
     <v-card :width="w" :height="h">
-        <monkey-monitor2 />
+        <monkey-monitor2 :active="isActive"/>
     </v-card>
 
 
--- resources/js/store/experiment2Store.js
+++ resources/js/store/experiment2Store.js
@@ -1,48 +1,91 @@
 import {defineStore} from "pinia";
+import {getRandom, SuperTimer} from "@mixins/utils.js";
 
 
 export const useExperiment2Store = defineStore('experiment2', {
-    state() {
+    state:()=> {
         return {
             active: false,
             comment: '',
-            position: [],
+            positions: [],
+            position: '',
             stimul: {
                 type: '',
                 frequency: 0,
                 length: 0,
             },
             helpers: [],
+            timer: null,
             line: {
                 countProbs: 3,
                 currentProb: 0,
-                startDelay: 0,
+                startDelay: 1000,
+                showHelpers: false,
+                crntHelper: {},
                 startHelp: {
-                    min: 0,
-                    max: 0
+                    min: 1000,
+                    max: 1000
                 },
                 waitQuestion: {
-                    min: 0,
-                    max: 0
+                    min: 1000,
+                    max: 1000
                 },
                 stopDelay: {
-                    min: 0,
-                    max: 0
+                    min: 1000,
+                    max: 1000
                 },
             }
         }
     },
     getters: {
-        getTrueValue(state){},
-        getReactionTime(state){}
+        getActive(state){return state.active}
     },
     actions: {
-        async runExperiment(){
-            this.active = true
-            while (this.line.currentProb < this.line.countProbs){
-                this.comment += "<p>kek</p>"
+        setActive(val){
+            this.active = val
+        },
+        async runExperiment() {
+
+            while (this.line.currentProb < this.line.countProbs) {
+                this.line.crntHelper = this.helpers.at(getRandom(0, this.helpers.length - 1))
+                this.position = this.positions.at(getRandom(0, this.positions.length - 1))
+                this.setActive(true)
+                this.comment += "<p>Сигнал</p>"
+                let t = new SuperTimer()
+                await t.timeout(() => {
+                    this.comment += "<p>Сигнал ждем</p>"
+                }, this.line.startDelay)
+                this.comment += "<p>стимул</p>"
+                t = new SuperTimer()
+                await t.timeout(() => {
+                    this.line.showHelpers = true
+                    this.comment += "<p>Пауза перед подсказкой</p>"
+                }, getRandom(this.line.startHelp.min, this.line.startHelp.max))
+                this.comment += "<p>стимул</p>"
+                this.timer = new SuperTimer()
+
+                await (async () => {
+                    try {
+                        await this.timer.timeout(() => {
+                            this.line.showHelpers = false
+                            this.comment += "<p>Пауза перед подсказкой(нажала)</p>"
+                        }, getRandom(this.line.waitQuestion.min, this.line.waitQuestion.max))
+                    } catch (e) {
+                        this.line.showHelpers = false
+                    }
+                })()
+                t = new SuperTimer()
+                this.comment += "<p>Пауза</p>"
+                await t.timeout(() => {
+                    this.comment += "<p>следующая</p>"
+                }, getRandom(this.line.stopDelay.min, this.line.stopDelay.max))
                 this.line.currentProb++
             }
+            this.setActive(false)
         }
+    },
+    share: {
+        enable: true,
+        initialize: true,
     }
 })
