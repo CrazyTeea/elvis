3e38e6983085c1790e17c66000fd9f808db972df
CrazyTea <lipatow.nikita@yandex.ru>
2024-06-25 17:10:05 +0300

    fixes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Laravel</title>
@@ -14,8 +15,15 @@
         }
 
     </style>
+
 </head>
 <body>
-    <div id="app"></div>
+<div id="app"></div>
 </body>
+<script>
+    window.addEventListener("wheel", e => {
+        if (e.ctrlKey)
+            e.preventDefault();//prevent zoom
+    }, {passive: false});
+</script>
 </html>
